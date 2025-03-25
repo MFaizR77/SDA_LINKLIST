3cb59bc95d5e5a8669a854e9a1fa415f69d6db6f
MFaizR77 <faizram777@gmail.com>
2025-03-25 11:13:24 +0700

    sda_3

--- header.h
+++ header.h
@@ -1,67 +1,39 @@
+#ifndef STACK_H
+#define STACK_H
 
-#ifndef header_h
-#define header_h
-#include "boolean.h"
 #include <stdio.h>
-#define Nil NULL
-#define Info(P) (P)->info
-#define Next(P) (P)->next
-#define First(L) (L).First
+#include <stdlib.h>
+#include "boolean.h"
+
 
 typedef int infotype;
+
+
 typedef struct tElmtList *address;
 typedef struct tElmtList {
-	 infotype info;
-	 address  next;
+    infotype info;
+    address next;
 } ElmtList;
 
 
 typedef struct {
-	  address First;
+    address First;
 } List;
 
 
-boolean ListEmpty (List L);
-
-void CreateList (List * L);
-
-address Alokasi (infotype X);
-
-void DeAlokasi (address P);
+typedef List Stack;
 
 
-address Search (List L, infotype X);
 
-boolean FSearch (List L, address P);
+boolean ListEmpty(List L);
 
-address SearchPrec (List L, infotype X);
 
-void InsVFirst (List * L, infotype X);
+address Alokasi(infotype X);
+void DeAlokasi(address P);
 
-void InsVLast (List * L, infotype X);
-
-void DelVFirst (List * L, infotype * X);
-
-void DelVLast (List * L, infotype * X);
-
-void InsertFirst (List * L, address P);
-
-void InsertAfter (List * L, address P, address Prec);
-
-void InsertLast (List * L, address P);
-
-void DelFirst (List * L, address * P);
-
-void DelP (List * L, infotype X);
-
-void DelLast (List * L, address * P);
-
-void DelAfter (List * L, address * Pdel, address Prec);
-
-void PrintInfo (List L);
-
-void DelAll (List * L);
-
-void DelAllGanjil (List * L);
+void PrintList(List L);
+void InsertFirst(List *L, infotype X);
+void DelFirst(List *L, infotype *X);
+void DecimalToBinary(int n);
 
 #endif
--- main.cpp
+++ main.cpp
@@ -1,90 +1,121 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "header.h"
+#include "stack.h"
 
 int main() {
     List L;
     infotype X;
-    int choice, value, precValue;
-    address Prec;  // Pindahkan deklarasi ke sini
-
-    CreateList(&L);
-    printf("List dibuat.\n");
-
+    int pilihan;
+    CreateEmpty(&L); 
+    
     do {
-        printf("\nMenu:\n");
-        printf("1. Insert First\n");
-        printf("2. Insert Last\n");
-        printf("3. Insert After\n");
-        printf("4. Delete First\n");
-        printf("5. Delete Last\n");
-        printf("6. Delete Value\n");
-        printf("7. Delete All Ganjil\n");
-        printf("8. Print List\n");
-        printf("9. Exit\n");
-        printf("Pilih operasi: ");
-        scanf("%d", &choice);
-
-        switch (choice) {
+        printf("\nMENU STACK LINKED LIST");
+        printf("\n1. Push (Tambah Elemen)");
+        printf("\n2. Exit");
+        printf("\nPilihan: ");
+        scanf("%d", &pilihan);
+        
+        switch (pilihan) {
             case 1:
-                printf("Masukkan nilai: ");
-                scanf("%d", &value);
-                InsVFirst(&L, value);
+                printf("Masukkan elemen: ");
+                scanf("%d", &X); 
+                DecimalToBinary(X); 
+                printf("Elemen %d telah ditambahkan ke stack.\n", X);
                 break;
             case 2:
-                printf("Masukkan nilai: ");
-                scanf("%d", &value);
-                InsVLast(&L, value);
-                break;
-            case 3:
-                printf("Masukkan nilai baru: ");
-                scanf("%d", &value);
-                printf("Masukkan nilai elemen sebelum nilai baru: ");
-                scanf("%d", &precValue);
-
-                Prec = First(L);
-                while (Prec != Nil && Info(Prec) != precValue) {
-                    Prec = Next(Prec);
-                }
-
-                if (Prec != Nil) {
-                    address P = Alokasi(value);
-                    if (P != Nil) {
-                        InsertAfter(&L, P, Prec);
-                    } else {
-                        printf("Gagal mengalokasikan memori!\n");
-                    }
-                } else {
-                    printf("Elemen %d tidak ditemukan.\n", precValue);
-                }
-                break;
-            case 4:
-                DelVFirst(&L, &X);
-                printf("Elemen pertama (%d) dihapus.\n", X);
-                break;
-            case 5:
-                DelVLast(&L, &X);
-                printf("Elemen terakhir (%d) dihapus.\n", X);
-                break;
-            case 6:
-                printf("Masukkan nilai yang ingin dihapus: ");
-                scanf("%d", &value);
-                DelP(&L, value);
-                break;
-            case 7:
-                DelAllGanjil(&L);
-                printf("Semua bilangan ganjil dihapus.\n");
-                break;
-            case 8:
-                PrintInfo(L);
-                break;
-            case 9:
                 printf("Keluar dari program.\n");
                 break;
             default:
                 printf("Pilihan tidak valid!\n");
         }
-    } while (choice != 9);
+    } while (pilihan != 2);
 
     return 0;
 }
+
+// int main() {
+//     List L;
+//     infotype X;
+//     int choice, value, precValue;
+//     address Prec;  // Pindahkan deklarasi ke sini
+
+//     CreateList(&L);
+//     printf("List dibuat.\n");
+
+//     do {
+//         printf("\nMenu:\n");
+//         printf("1. Insert First\n");
+//         printf("2. Insert Last\n");
+//         printf("3. Insert After\n");
+//         printf("4. Delete First\n");
+//         printf("5. Delete Last\n");
+//         printf("6. Delete Value\n");
+//         printf("7. Delete All Ganjil\n");
+//         printf("8. Print List\n");
+//         printf("9. Exit\n");
+//         printf("Pilih operasi: ");
+//         scanf("%d", &choice);
+
+//         switch (choice) {
+//             case 1:
+//                 printf("Masukkan nilai: ");
+//                 scanf("%d", &value);
+//                 InsVFirst(&L, value);
+//                 break;
+//             case 2:
+//                 printf("Masukkan nilai: ");
+//                 scanf("%d", &value);
+//                 InsVLast(&L, value);
+//                 break;
+//             case 3:
+//                 printf("Masukkan nilai baru: ");
+//                 scanf("%d", &value);
+//                 printf("Masukkan nilai elemen sebelum nilai baru: ");
+//                 scanf("%d", &precValue);
+
+//                 Prec = First(L);
+//                 while (Prec != Nil && Info(Prec) != precValue) {
+//                     Prec = Next(Prec);
+//                 }
+
+//                 if (Prec != Nil) {
+//                     address P = Alokasi(value);
+//                     if (P != Nil) {
+//                         InsertAfter(&L, P, Prec);
+//                     } else {
+//                         printf("Gagal mengalokasikan memori!\n");
+//                     }
+//                 } else {
+//                     printf("Elemen %d tidak ditemukan.\n", precValue);
+//                 }
+//                 break;
+//             case 4:
+//                 DelVFirst(&L, &X);
+//                 printf("Elemen pertama (%d) dihapus.\n", X);
+//                 break;
+//             case 5:
+//                 DelVLast(&L, &X);
+//                 printf("Elemen terakhir (%d) dihapus.\n", X);
+//                 break;
+//             case 6:
+//                 printf("Masukkan nilai yang ingin dihapus: ");
+//                 scanf("%d", &value);
+//                 DelP(&L, value);
+//                 break;
+//             case 7:
+//                 DelAllGanjil(&L);
+//                 printf("Semua bilangan ganjil dihapus.\n");
+//                 break;
+//             case 8:
+//                 PrintInfo(L);
+//                 break;
+//             case 9:
+//                 printf("Keluar dari program.\n");
+//                 break;
+//             default:
+//                 printf("Pilihan tidak valid!\n");
+//         }
+//     } while (choice != 9);
+
+//     return 0;
+// }
